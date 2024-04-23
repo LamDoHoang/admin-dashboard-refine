f018b09634981e5921b7694033a3dab10993519c
LamDoHoang <irokatsuna@gmail.com>
2024-04-23 10:19:54 +0700

    Add Deal chart using ant plots

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,6 +5,17 @@ import React from 'react'
 export const Home = () => {
   return (
     <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} sm={24} xl={8}>
+          
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+
+        </Col>
+      </Row>
       <Row
         gutter={[32, 32]}
         style={{
@@ -25,7 +36,7 @@ export const Home = () => {
         <Col
           xs={24}
           sm={24}
-          xl={8}
+          xl={16}
           style={{
             height: '460px'
           }}
