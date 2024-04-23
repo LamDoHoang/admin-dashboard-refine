0c4d254097ff77eb3327e4f6cdfcb96f6bbc4c10
LamDoHoang <irokatsuna@gmail.com>
2024-04-23 11:37:24 +0700

    Add lastest activities to dashboard

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,42 @@
-import { DashboardDealChart, UpcomingEvents } from '@/components'
+import { DashboardDealChart, DashboardTotalCountCard, UpcomingEvents, LatestActivities } from '@/components'
+import { DASHBOARD_TOTAL_COUNTS_QUERY } from '@/graphql/queries'
+import { DashboardTotalCountsQuery } from '@/graphql/types'
+import { useCustom } from '@refinedev/core'
 import { Col, Row } from 'antd'
 import React from 'react'
 
 export const Home = () => {
+  const { data, isLoading } = useCustom<DashboardTotalCountsQuery>({
+    url: '',
+    method: 'get',
+    meta: {
+      gqlQuery: DASHBOARD_TOTAL_COUNTS_QUERY
+    }
+  })
+
   return (
     <div>
       <Row gutter={[32, 32]}>
         <Col xs={24} sm={24} xl={8}>
-          
+          <DashboardTotalCountCard
+            resources="companies"
+            isLoading={isLoading}
+            totalCount={data?.data.companies.totalCount}
+          />
         </Col>
         <Col xs={24} sm={24} xl={8}>
-
+          <DashboardTotalCountCard
+            resources="contacts"
+            isLoading={isLoading}
+            totalCount={data?.data.contacts.totalCount}
+          />
         </Col>
         <Col xs={24} sm={24} xl={8}>
-
+          <DashboardTotalCountCard
+            resources="deals"
+            isLoading={isLoading}
+            totalCount={data?.data.deals.totalCount}
+          />
         </Col>
       </Row>
       <Row
@@ -42,6 +65,16 @@ export const Home = () => {
           }}
         >
           <DashboardDealChart />
+        </Col>
+      </Row>
+      <Row
+        gutter={[32, 32]}
+        style={{
+          marginTop: '32px'
+        }}
+      >
+        <Col xs={24}>
+          <LatestActivities />
         </Col>
       </Row>
     </div>
